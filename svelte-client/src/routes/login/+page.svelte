fc67513e0fff8c92bec38286258a0d2e6b03b45e
Philippe Martin <philippeoliviermartin@gmail.com>
2024-12-08 20:12:03 -0500

    added login page

---
+++ svelte-client/src/routes/login/+page.svelte
@@ -0,0 +1,13 @@
+<div class="login-container">
+    <h1>Login</h1>
+</div>
+
+<style>
+    .login-container {
+        display: flex;
+        align-items: center;
+
+        height: 100%;
+        width: 100%;
+    }
+</style>
