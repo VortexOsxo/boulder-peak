317ef5f8283e7f1ddf39f3a22806310a8cbb9f45
Philippe Martin <philippeoliviermartin@gmail.com>
2024-12-24 10:59:39 -0500

    added start workout button

--- svelte-client/src/routes/(app)/workout/+page.svelte
+++ svelte-client/src/routes/(app)/workout/+page.svelte
@@ -2,6 +2,7 @@
     import { onMount } from "svelte";
     import { getAuthorizationHeader } from "$lib";
     import ExerciseList from "$lib/components/exercise-list.svelte";
+    import { workoutState } from "$lib/stores/workout";
 
     let message = "";
 
@@ -9,10 +10,19 @@
         fetch("http://127.0.0.1:5000/training/", {
             method: "GET",
             headers: { Authorization: getAuthorizationHeader() },
-        }).then((response) =>
-            response.text().then((text) => message = text),
-        ),
+        }).then((response) => response.text().then((text) => (message = text))),
     );
 </script>
 
-<ExerciseList/>
+{#if !workoutState.inWorkout}
+    <ExerciseList />
+
+    <button
+        class="fixed bottom-32 left-1/2 transform -translate-x-1/2 z-25 bg-accent text-black px-4 py-2 rounded"
+        onclick={() => (workoutState.inWorkout = true)}
+    >
+        Start Workout
+    </button>
+{:else}
+    <h2>Workout Session</h2>
+{/if}
