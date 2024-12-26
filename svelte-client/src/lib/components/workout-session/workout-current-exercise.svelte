4d6a7879cda872927068ec5edd9c4041a782ba64
Philippe Martin <philippeoliviermartin@gmail.com>
2024-12-26 16:34:14 -0500

    improved sets reactivity

--- svelte-client/src/lib/components/workout-session/workout-current-exercise.svelte
+++ svelte-client/src/lib/components/workout-session/workout-current-exercise.svelte
@@ -1,42 +1,55 @@
 <script>
     import { workoutState, initializeSets, addSet } from "$lib/stores/workout";
+    import { Checkbox } from "flowbite-svelte";
+    import { onDestroy } from "svelte";
     import { get } from "svelte/store";
 
     const { currentExercise } = workoutState;
     let sets = initializeSets(get(currentExercise));
 
+    const unsubscribe = currentExercise.subscribe((value) => {
+        sets = initializeSets(value);
+    });
+
+    onDestroy(() => {
+        unsubscribe();
+    });
+
     function handleAddSet() {
         addSet(get(currentExercise));
-        sets = [...initializeSets(get(currentExercise))]; // Reassign the sets array to trigger reactivity
+        sets = [...initializeSets(get(currentExercise))];
     }
 </script>
 
-<p class="text-title-text">{$currentExercise.name}</p>
-{#each sets as set, index}
-    <div class="flex items-center mb-4">
-        <p class="mr-4 text-title-text">Set {index + 1}</p>
-        <label class="flex items-center">
-            <input
-                type="number"
-                bind:value={set.weight}
-                class="border rounded p-1 bg-secondary-background text-title-text w-16"
-            />
-            <span class="mr-2 text-title-text m-1"> lb </span>
-        </label>
-        <label class="flex items-center ml-4">
-            <input
-                type="number"
-                bind:value={set.reps}
-                class="border rounded p-1 bg-secondary-background text-title-text w-16"
-            />
-            <span class="mr-2 text-title-text m-1"> reps </span>
-        </label>
-    </div>
-{/each}
+<div class="flex flex-col items-center">
+    <p class="text-title-text">{$currentExercise.name}</p>
+    {#each sets as set, index}
+        <div class="flex items-center mb-4">
+            <p class="mr-4 text-title-text">Set {index + 1}</p>
+            <label class="flex items-center">
+                <input
+                    type="number"
+                    bind:value={set.weight}
+                    class="border rounded p-1 bg-secondary-background text-title-text w-16"
+                />
+                <span class="mr-2 text-title-text m-1"> lb </span>
+            </label>
+            <label class="flex items-center ml-4">
+                <input
+                    type="number"
+                    bind:value={set.reps}
+                    class="border rounded p-1 bg-secondary-background text-title-text w-16"
+                />
+                <span class="mr-2 text-title-text m-1"> reps </span>
+            </label>
+            <Checkbox bind:checked={set.done} class="ml-4"></Checkbox>
+        </div>
+    {/each}
 
-<button
-    class="bg-accent text-white px-4 py-2 rounded"
-    onclick={handleAddSet}
->
-    Add Set
-</button>
+    <button
+        class="bg-accent text-white px-4 py-2 rounded"
+        onclick={handleAddSet}
+    >
+        Add Set
+    </button>
+</div>
