136e21482e3f439f4885e9e50bb9c08330a50ce8
Philippe Martin <philippeoliviermartin@gmail.com>
2024-12-28 17:02:55 -0500

    added route to get workout from user

--- server/flaskr/blueprints/workout.py
+++ server/flaskr/blueprints/workout.py
@@ -7,7 +7,7 @@ from .auth import login_required
 
 @workout_bp.route("/", methods=["POST"])
 @login_required
-def workout():
+def log_workout():
     data = request.get_json()
     data['user_id'] = g.user_id
     
@@ -16,3 +16,11 @@ def workout():
     workouts_collection.insert_one(data)
 
     return "Workout logged", 201
+
+@workout_bp.route("/", methods=["GET"])
+@login_required
+def get_workjouts():
+    db = get_db()
+    workouts_collection = db['workouts']
+    workouts = workouts_collection.find({'user_id': g.user_id})
+    return {"workouts": list(workouts)}, 200
