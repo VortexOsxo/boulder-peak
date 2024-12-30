f4062dbd36b206430b9dd2a266728a8d8f937745
Philippe Martin <philippeoliviermartin@gmail.com>
2024-12-29 20:48:16 -0500

    replaced exercise in selecter with the store

--- svelte-client/src/lib/components/exercise-selecter.svelte
+++ svelte-client/src/lib/components/exercise-selecter.svelte
@@ -1,43 +1,15 @@
 <script>
     import { get } from "svelte/store";
     import { Checkbox } from "flowbite-svelte";
-    import { exercises, updateExercisesWithSelected } from "$lib/stores/exercices";
+    import {
+        exerciseData,
+        exercises,
+        updateExercisesWithSelected,
+    } from "$lib/stores/exercices";
 
     let selectedExercises = new Set();
 
     let { closeCallback } = $props();
-
-    const exercises_name = [
-        "Dumbbell Curl",
-        "Pull Up",
-        "Bench Press",
-        "Squat",
-        "Leg press",
-        "Dips",
-        "Lats pulldown",
-        "Dumbell pec fly",
-        "Tricep extension",
-        "Leg curl",
-        "Leg extension",
-        "Shoulder press",
-        "Deadlift",
-        "Calf raise",
-        "Barbell curl",
-        "Tricep pushdown",
-        "Bent over row",
-        "Seated row",
-        "Incline bench press",
-        "Decline bench press",
-        "Chest press",
-        "Shoulder fly",
-        "Front raise",
-        "Lateral raise",
-        "Reverse fly",
-        "Shrug",
-        "Crunch",
-        "Leg raise",
-        "Russian twist",
-    ];
 
     selectedExercises.clear();
     get(exercises).forEach((exercise) => selectedExercises.add(exercise.name));
@@ -51,10 +23,10 @@
 </script>
 
 <div class="overflow-y-auto h-full no-scrollbar">
-    {#each exercises_name as exercise, index}
+    {#each $exerciseData as exercise, index}
         <div class="flex items-center justify-between">
             <h3 class="text-xl font-semibold text-title-text m-4">
-                {exercise}
+                {exercise.name}
             </h3>
             <Checkbox
                 class="m-4"
@@ -62,7 +34,7 @@
                 on:change={() => toggleSelection(exercise)}
             />
         </div>
-        {#if index < exercises_name.length - 1}
+        {#if index < $exerciseData.length - 1}
             <hr class="border-t border-gray-300" />
         {/if}
     {/each}
@@ -71,10 +43,10 @@
 
 <button
     class="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-25 bg-accent text-white px-4 py-2 rounded"
-    onclick="{() => {
+    onclick={() => {
         updateExercisesWithSelected(selectedExercises);
         closeCallback();
-    }}"
+    }}
 >
     Add exercises
 </button>
--- svelte-client/src/lib/interfaces/exercise.ts
+++ svelte-client/src/lib/interfaces/exercise.ts
@@ -12,4 +12,10 @@ export interface Exercise {
     weight: number;
 }
 
+export interface ExerciseTarget {
+    sets: number;
+    reps: number;
+    weight: number;
+}
+
 export const defaultExercise: Exercise = {name: "", reps: 0, sets: 0, weight: 0};
